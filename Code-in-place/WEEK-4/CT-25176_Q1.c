4c2da1d78f2e76e303569fb8340441905f3bd444
Aryan Ahmed <aryanahmed03.2005@gmail.com>
2025-09-23 00:09:29 +0500

    week 4 code in place

---
+++ Code-in-place/WEEK-4/CT-25176_Q1.c
@@ -0,0 +1,27 @@
+#include <stdio.h> 
+
+//LEAP YEAR CHECKER 
+
+int main(){ 
+
+    int year; 
+
+    printf("Enter YEAR :"); 
+
+    scanf("%d", &year); 
+
+  
+
+    if(year % 400 == 0 || (year % 4 == 0 && year % 100 != 0) ){  
+
+        printf("This is a leap year \n"); 
+
+    }else{ 
+
+        printf("This is not a leap year \n"); 
+
+    } 
+
+    return 0; 
+
+} 
