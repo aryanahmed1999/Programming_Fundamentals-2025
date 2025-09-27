799ca5c93d8ac623e5c9d24dd24bd377511cf75e
Aryan Ahmed <aryanahmed03.2005@gmail.com>
2025-09-28 00:04:23 +0500

    Update Exercise_4.c

--- Lab_task_5/Exercise_4.c
+++ Lab_task_5/Exercise_4.c
@@ -1,5 +1,14 @@
 #include <stdio.h>
-
+/*Using IF and Switch statement, write a program that displays the following menu for the food
+items available to take order from the customer:
+B= Burger (Rs. 200)
+F= French Fries (Rs. 50)
+P= Pizza (Rs. 500)
+S= Sandwiches (Rs. 150)
+The costumer can order any combination of available food. The program first ask to enter the
+no of types of snacks i.e. 2, 3 or 4 then it ask to enter the choice i.e. B for Burger and then for
+quantity.8/
+    
 void menu();
 int main(){
     menu();
